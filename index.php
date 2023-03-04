4af8fe07b30baf7a3a17affcc5de3e126b35ddce
Peter Dias <peterdias77@gmail.com>
2023-03-04 09:16:34 +0530

    fix

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "Login Page" 
+$fulldomain = $_SERVER['SERVER_NAME'];
+$matches = null; 
+preg_match('/^[^.]+/', $fulldomain, $matches);
+$subdomain = $matches[0];
+
+echo $subdomain." Login Page";
 
 ?>
