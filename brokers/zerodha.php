8453ae7444ce9f09c0f9a616fab94d9b7f260daa
Peter Dias <peterdias77@gmail.com>
2023-03-04 09:31:47 +0530

    fix

--- brokers/zerodha.php
+++ brokers/zerodha.php
@@ -1,5 +1,22 @@
 <?php
 
-echo "Zerodha Login Page"
+$action = $_REQUEST['action']
+$uid = $_REQUEST['uid']
+$aid = $_REQUEST['aid']
+
+if($action == 'login')
+{
+    echo "Zerodha Login Page "
+    echo "<p>"
+    echo $uid
+}
+else if($action == 'checkout')
+{
+    echo 'Zerodha Checkout page'
+}
+else 
+{
+    echo "Error"
+}
 
 ?>
